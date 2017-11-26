87c39047d38ed9679d4e6ecb7d567ea5e169f8d2
eko-malt <kostya.kokosko@gmail.com>
2017-11-26 17:25:14 +0200

    oops.. fix something

--- db/migrate/20171125084920_create_providers.rb
+++ db/migrate/20171125084920_create_providers.rb
@@ -2,7 +2,7 @@ class CreateProviders < ActiveRecord::Migration[5.1]
   def change
     create_table :providers do |t|
       t.string :name
-
+      t.integer :gtype
       t.timestamps
     end
   end
--- db/migrate/20171125164605_add_type_to_providers.rb
+++
@@ -1,5 +0,0 @@
-class AddTypeToProviders < ActiveRecord::Migration[5.1]
-  def change
-    add_column :providers, :gtype, :integer
-  end
-end
